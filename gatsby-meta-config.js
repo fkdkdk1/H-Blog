0f0133c40a21718f58b283fb7b5e128500ff8309
fkdkdk1 <fkdkdk3@daum.net>
2020-04-22 19:39:13 +0900

    에러 해결 (2)

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -3,7 +3,7 @@ module.exports = {
   description: `Blog posted about ...`,
   author: 'Han Jae Hyeob',
   introduction: `Welcome to my blog`,
-  siteUrl: `http://fkdkdk1.github.io`, // Your blog site url
+  siteUrl: `https://fkdkdk1.github.io/`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
     github: `fkdkdk1`, // Your GitHub account
@@ -15,13 +15,13 @@ module.exports = {
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'fkdkdk1/fkdkdk1.github.io', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'jbee',
+    buyMeACoffeeId: 'fkdkdk1',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
